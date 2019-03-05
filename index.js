8b94cb790a80ca077c8b412a89c29ae64b713f11
picachuwu <picachuwu@e-formula.com>
2019-03-05 12:18:19 +0800

    array init

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ function readHoldingRegisters(from,to,reply) {
 
 function readCoils(from,to,reply) {
     console.log('Read coils '+from+'-'+to);
-    var values = []; // anything greater than zero is received as a 1
-    values.fill(1, 0, to-from+1);//(value, start, end)
+    var values = new Array(to-from+1); // anything greater than zero is received as a 1
+    values.fill(1, 0, values.length);//(value, start, end)
         // for (var i = (from % 2); i < values.length; i+=2){
     //     values[i] = 0;
     // }
