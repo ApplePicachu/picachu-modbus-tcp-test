441f66d08e087026047219bf71a03ed5d3ddc8d3
picachuwu <picachuwu@e-formula.com>
2019-03-05 12:19:14 +0800

    change value

--- index.js
+++ index.js
@@ -5,47 +5,47 @@ const port = 502;
 
 var tcpServer = net.createServer();
 
-tcpServer.listen(port,function(){
-   console.log('TCP Socket bound to port '+port);
+tcpServer.listen(port, function () {
+    console.log('TCP Socket bound to port ' + port);
 });
 
-tcpServer.on('connection', function(socket){
+tcpServer.on('connection', function (socket) {
     console.log('client has connected');
     modbusServer.pipe(socket);
 
-    socket.on('error', function(e){
-        console.log('Connection error: '+e);
+    socket.on('error', function (e) {
+        console.log('Connection error: ' + e);
         socket.destroy();
     });
 
-    socket.on('close', function(e){
+    socket.on('close', function (e) {
         console.log('Client has closed connection.');
     });
 });
 
 modbusServer.on('read-holding-registers', readHoldingRegisters);
-modbusServer.on('read-coils',readCoils);
-modbusServer.on('write-multiple-registers',writeRegisters);
+modbusServer.on('read-coils', readCoils);
+modbusServer.on('write-multiple-registers', writeRegisters);
 
-function readHoldingRegisters(from,to,reply) {
-    console.log('Read holding registers '+from+'-'+to);
-    var values = [1,6,3,9]; // sample values just to see if it works.
-    return reply(null,bufferify(values));
+function readHoldingRegisters(from, to, reply) {
+    console.log('Read holding registers ' + from + '-' + to);
+    var values = [1, 6, 3, 9]; // sample values just to see if it works.
+    return reply(null, bufferify(values));
 }
 
-function readCoils(from,to,reply) {
-    console.log('Read coils '+from+'-'+to);
-    var values = new Array(to-from+1); // anything greater than zero is received as a 1
+function readCoils(from, to, reply) {
+    console.log('Read coils ' + from + '-' + to);
+    var values = new Array(to - from + 1); // anything greater than zero is received as a 1
     values.fill(1, 0, values.length);//(value, start, end)
-        // for (var i = (from % 2); i < values.length; i+=2){
-    //     values[i] = 0;
-    // }
+    for (var i = (from % 2); i < values.length; i += 2) {
+        values[i] = 0;
+    }
     console.log(JSON.stringify(values));
-    return reply(null,values);
+    return reply(null, values);
 }
 
-function writeRegisters(from,to,items,reply) {
-    console.log('Write registers '+from+'-'+to);
+function writeRegisters(from, to, items, reply) {
+    console.log('Write registers ' + from + '-' + to);
     //console.log('  items:'+items);
     reply();
 }
@@ -55,9 +55,9 @@ function bufferify(itemsArray) {
     // array of Buffers (not just an array of numbers) to the reply function.
     var n = itemsArray.length;
     var registers = [];
-    for (var i=0; i<n; i++) {
+    for (var i = 0; i < n; i++) {
         registers[i] = Buffer.alloc(2);
-        registers[i].writeInt16BE(itemsArray[i],0);
+        registers[i].writeInt16BE(itemsArray[i], 0);
     }
     return registers;
 }
